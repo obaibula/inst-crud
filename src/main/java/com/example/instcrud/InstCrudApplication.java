32f69da21c7cf2f6a4c2b3f5acf9189a746fa22a
oleh27yo <baibula.oo@gmail.com>
2023-05-19 12:28:50 +0300

    init

---
+++ src/main/java/com/example/instcrud/InstCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.instcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/instcrud/InstCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
