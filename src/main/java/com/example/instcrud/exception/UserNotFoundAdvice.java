899265689f6528e73f6cf37decd27c87938e4c6a
oleh27yo <baibula.oo@gmail.com>
2023-05-19 17:21:43 +0300

    refactor

--- src/main/java/com/example/instcrud/exception/UserNotFoundAdvice.java
+++ src/main/java/com/example/instcrud/exception/UserNotFoundAdvice.java
@@ -5,14 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class UserNotFoundAdvice {
 
-    @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<UserErrorResponse> userNotFoundHandler(UserNotFoundException e){
         UserErrorResponse response = new UserErrorResponse();
 
