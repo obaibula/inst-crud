18efbbae7d8be80879826bb524d83fdb7f4a8fcd
Oleh Baibula <baibula.oo@gmail.com>
2023-06-03 07:21:22 +0300

    commit
    Signed-off-by: obaibula <baibula.oo@gmail.com>

--- src/main/java/com/example/instcrud/exception/PostNotFoundException.java
+++ src/main/java/com/example/instcrud/exception/PostNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.instcrud.exception;
 
+//test
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(String message) {
         super(message);
