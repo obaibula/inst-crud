b7c0b0ac60065412385a5a00a4b6ee9aba891a00
obaibula <baibula.oo@gmail.com>
2023-06-11 08:47:15 +0300

    struggling with tests

--- src/main/resources/db/migration/V1.3.1__populate-users.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (username, bio, avatar, email, phone, password, status)
-VALUES
-('oleh1234', 'hello bio', 'http://avatar1.com', 'oleh@mail.ua', '+38 050 133-95-31', 'passWord123^&+=', 'ONLINE'),
-('sasha777', 'good bye', 'http://avatar2.com', null, '+38 033 223-13-41', '@#$%^&+=Aa1', 'OFFLINE'),
-('vasya912', 'Ive never been ok', 'http://avatar3.com', 'vasya@gmail.com', '+38 067 256-11-12', 'Small1#%$', 'ONLINE'),
-('petro3345', null, 'http://avatar4.com', 'petya@mail.ua', null, 'Small1#%$', 'OFFLINE'),
-('belyysaa', null, 'http://avatar5.com', 'bel@gmail.ua', null, 'Small1#^$', 'ONLINE'),
-('gorgonzola', 'hello bio', null, null, '+38 066 121-12-51', 'SmaLL1^%$', 'OFFLINE');
--- src/test/java/com/example/instcrud/repository/UserRepositoryTest.java
+++ src/test/java/com/example/instcrud/repository/UserRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.example.instcrud.repository;
 
 import com.example.instcrud.entity.Post;
+import com.example.instcrud.entity.User;
+import com.example.instcrud.entity.UserStatus;
 import com.example.instcrud.exception.UserNotFoundException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,7 +35,7 @@ class UserRepositoryTest {
     }
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository underTest;
     @Autowired
     private PostRepository postRepository;
 
@@ -38,19 +43,17 @@ class UserRepositoryTest {
     private CommentRepository commentRepository;
 
     // todo: change the behavior of the population of the db in containers!
-    /*@AfterEach
+    @AfterEach
     void tearDown() {
-        System.err.println("THE PROBLEM IS HERE******************************");
-        userRepository.deleteAll();
-        System.err.println("THE PROBLEM IS HERE**************************");
-    }*/
+        underTest.deleteAllInBatch();
+    }
 
     @Test
     void shouldFindAllUsersAndFetchTheirsPostsEagerly() {
         // given
 
         Long userId = 1L;
-        var user = userRepository.findAllFetchPosts(Pageable.unpaged())
+        var user = underTest.findAllFetchPosts(Pageable.unpaged())
                 .get()
                 .filter(u -> u.getId().equals(userId))
                 .findFirst()
@@ -68,15 +71,21 @@ class UserRepositoryTest {
 
     @Test
     @Transactional
-    void shouldDeleteAllUsersAndTheirsOrphansInBulk(){
+    void shouldDeleteAllUsersAndTheirsOrphansInBulk() {
+        User user = createRandomUser();
+        var post = createRandomPost();
+        user.addPost(post);
+        post.setUser(user);
+        underTest.save(user);
+        System.err.println(postRepository.findAllByUserId(100l, Pageable.unpaged()));
         Long userId = 4L;
         Long postId = 11L; // post that belongs to the user with id 4;
         Long commentIdByPost = 20L; // comment that belongs to the post with id 11;
         Long commentIdByUser = 7L; // comment that belongs to the user with id 4;
 
-        userRepository.deleteInBulkById(userId);
+        underTest.deleteInBulkById(userId);
 
-        var userOptional = userRepository.findById(userId);
+        var userOptional = underTest.findById(userId);
         var postOptional = postRepository.findById(postId);
         var postsCommentOptional = commentRepository.findById(commentIdByPost);
         var usersCommentOptional = commentRepository.findById(commentIdByUser);
@@ -86,6 +95,56 @@ class UserRepositoryTest {
         assertThat(postsCommentOptional.isEmpty()).isTrue();
         assertThat(usersCommentOptional.isEmpty()).isTrue();
 
+    }
+
+    private Post createRandomPost() {
+        var post = new Post();
+        int i = ThreadLocalRandom.current().nextInt(0, 100);
+
+        post.setUrl("http://post.com" + i);
+        post.setCaption("the caption" + i);
+        post.setLat(ThreadLocalRandom.current().nextFloat(-90, 90));
+        post.setLng(ThreadLocalRandom.current().nextFloat(-180, 180));
+        return post;
+    }
+
+    private User createRandomUser() {
+        return User.builder()
+                .username(getRandomUsername())
+                .bio("test bio")
+                .avatar("http://avatar.jpeg")
+                .phone(getRandomPhone())
+                .email(getRandomEmail())
+                .password(getRandomPassword())
+                .status(getRandomStatus())
+                .build();
+    }
+
+    private UserStatus getRandomStatus() {
+        if(ThreadLocalRandom.current().nextBoolean())
+            return UserStatus.ONLINE;
+        else
+            return UserStatus.OFFLINE;
+    }
+
+    private String getRandomPassword() {
+        return "passWORD#" + ThreadLocalRandom.current().nextInt(1, 10000);
+    }
+
+    private String getRandomEmail() {
+        return "test"
+                + ThreadLocalRandom.current().nextInt(1, 99)
+                + "@mail.com";
+
+    }
+
+    private String getRandomPhone() {
+        return """
+                +38 050 133-95-""" + ThreadLocalRandom.current().nextInt(10, 99);
+    }
+
+    private String getRandomUsername() {
+        return "testuser" + ThreadLocalRandom.current().nextInt(1, 99);
     }
 
 }
