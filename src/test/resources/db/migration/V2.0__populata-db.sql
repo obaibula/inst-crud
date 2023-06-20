106b2a14724b3d925479e55ceaa0b0f01b690cfc
obaibula <baibula.oo@gmail.com>
2023-06-20 08:02:41 +0300

    added todo

---
+++ src/test/resources/db/migration/V2.0__populata-db.sql
@@ -0,0 +1,4 @@
+-- todo: insert appropriate values, instead of using java code
+
+INSERT INTO users(username, email, password, status)
+VALUES ('oleh12345', 'oleh@mail.com', 'jdhaJHJ23##', 'ONLINE');
